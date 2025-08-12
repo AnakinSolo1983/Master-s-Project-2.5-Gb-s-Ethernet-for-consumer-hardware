3059a1d6444b6642413e5b963263a052d5f51d59
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-12 19:12:26 +0100

    Add files via upload

---
+++ scripts/pnew.py
@@ -0,0 +1,35 @@
+from ipaddress import IPv4Address, IPv4Network
+import sys
+
+#print(sys.argv[1])
+#print(sys.argv[2])
+outp = str(sys.argv[2]) + "_mod_rule"
+print(outp)
+with open(str(sys.argv[1]), 'r') as file:
+	with open(outp, 'w') as output_file:
+    		for line in file:
+        		parts = line.strip().split('\t')
+        		source = parts[0][1:]  # Remove the '@'
+        		dest = parts[1]
+        		source_ports = parts[2].replace(' : ', ':')
+        		dest_ports = parts[3].replace(' : ', ':')
+        		if IPv4Address('192.168.11.10') in IPv4Network(source) and IPv4Address('192.168.11.1') in IPv4Network(dest):
+        			continue
+        
+        		if source == '0.0.0.0/0' and source == dest:
+        			continue
+        
+        		val1,val2 = dest_ports.split(':')
+        		if int(val1) != int(val2):
+                		if int(val1) != 0 or int(val2) != 65535:
+                	        	continue
+        
+        		output_file.write(line)
+        # Generate iptables rule
+        #rule = f"iptables -A INPUT -s {source} -d {dest} -p tcp --sport {source_ports} --dport {dest_ports} -j ACCEPT"
+        #rule = f"-A INPUT -s {source} -d {dest} -p tcp --sport {source_ports} --dport {dest_ports} -j DROP"
+        #"""
+        #if source == '0.0.0.0/0' and source == dest:
+        	#rule = f"-A INPUT -s {source} -d {dest} -p tcp --sport {source_ports} --dport {dest_ports} -j ACCEPT"
+        #"""
+        #print(rule)
