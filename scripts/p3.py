f82f0a05ed6a9bffe6fc3d3eec71b5367ee16188
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-13 15:01:07 +0100

    Add files via upload

---
+++ scripts/p3.py
@@ -0,0 +1,12 @@
+with open('classbench-ng/rules.3', 'r') as file:
+    for line in file:
+        parts = line.strip().split('\t')
+        source = parts[0][1:]  # Remove the '@'
+        dest = parts[1]
+        source_ports = parts[2].replace(' : ', ':')
+        dest_ports = parts[3].replace(' : ', ':')
+        
+        # Generate iptables rule
+        #rule = f"iptables -A INPUT -s {source} -d {dest} -p tcp --sport {source_ports} --dport {dest_ports} -j ACCEPT"
+        rule = f"-A INPUT -s {source} -d {dest} -p tcp --sport {source_ports} --dport {dest_ports} -j ACCEPT"
+        print(rule)
---
+++ scripts/p5k.py
@@ -0,0 +1,12 @@
+with open('dts/dep/test-acl-input/acl1v4_5k_rule', 'r') as file:
+    for line in file:
+        parts = line.strip().split('\t')
+        source = parts[0][1:]  # Remove the '@'
+        dest = parts[1]
+        source_ports = parts[2].replace(' : ', ':')
+        dest_ports = parts[3].replace(' : ', ':')
+        
+        # Generate iptables rule
+        #rule = f"iptables -A INPUT -s {source} -d {dest} -p tcp --sport {source_ports} --dport {dest_ports} -j ACCEPT"
+        rule = f"-A INPUT -s {source} -d {dest} -p tcp --sport {source_ports} --dport {dest_ports} -j ACCEPT"
+        print(rule)
