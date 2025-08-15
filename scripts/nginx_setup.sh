e4e8f77aa9961c379827896fb55a4681303bff6b
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 12:42:32 +0100

    Update nginx_setup.sh

--- scripts/nginx_setup.sh
+++ scripts/nginx_setup.sh
@@ -4,26 +4,26 @@
 
 #----------
 
-# Create namespace ns1.
+# Create namespace ns1:
 ip netns add ns1
 
-# Move the network interface enp87s0 into the namespace ns1.
+# Move the network interface enp87s0 into the namespace ns1:
 ip link set dev enp87s0 netns ns1
 
-# Bring the interface enp87s0 up within the namespace ns2.
+# Bring the interface enp87s0 up within the namespace ns2:
 ip netns exec ns1 ip link set dev enp87s0 up
 
-# Assign an IP address to the interface enp87s0 in the namespace ns1.
+# Assign an IP address to the interface enp87s0 in the namespace ns1:
 ip netns exec ns1 ip addr add dev enp87s0 192.168.11.1/24
 
-# Create namespace ns2.
+# Create namespace ns2:
 ip netns add ns2
 
-# Move the network interface enp88s0 into the namespace ns2.
+# Move the network interface enp88s0 into the namespace ns2:
 ip link set dev enp88s0 netns ns2
 
-# Bring the interface enp88s0 up within the namespace ns2.
+# Bring the interface enp88s0 up within the namespace ns2:
 ip netns exec ns2 ip link set dev enp88s0 up
 
-# Assign an IP address to the interface enp88s0 in the namespace ns2.
+# Assign an IP address to the interface enp88s0 in the namespace ns2:
 ip netns exec ns2 ip addr add dev enp88s0 192.168.11.10/24
