3203219b71bc03c05e501714f2a9313a9a69b3f7
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 12:31:15 +0100

    Update netflt.sh

--- scripts/netflt.sh
+++ scripts/netflt.sh
@@ -1,12 +1,15 @@
-NUM=${1}
-#COUNT=0;
-#python3 p${NUM}_c.py | while read line; do
+#----------
+
+# This Bash script is used to write down rules into iptables within a VM, which acts as server. As a parameter, it takes the variable NUM, which is the number for one of the rule sets that it to be used in testing.
+
+#----------
+
+NUM=${1} # Which rule set.
+
+# Perform a while loop, reading each line of rules from output:
 python3 p${NUM}.py | while read line; do
     iptables $line
-    #((COUNT++))
-    #echo $COUNT
 done
 
-#echo $COUNT
+# Load the rules into iptables:
 iptables -L -v -n > rulesN_r${NUM}.1
-#ip netns exec ns1 sudo iptables -F INPUT
