341ea034e305943d0029ba35aa0ab5e9501b2c0a
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 13:24:09 +0100

    Update perf_get.sh

--- scripts/perf_get.sh
+++ scripts/perf_get.sh
@@ -1,4 +1,13 @@
-RSF=${1}
-NGINXP=$(<nginxp.txt)
+#----------
+
+# This Bash script captures the performance data for nginx in the Virtual Machine, which acts as server.
+
+#----------
+
+RSF=${1} # Path to where the data is stored.
+
+NGINXP=$(<nginxp.txt) # Extract the nginx process ID.
 sleep 3
+
+# Record performance data for CPU cores 0 to 3, outputting to file perf.data. The command sleeps for 10 seconds while performance data is collected.
 perf record -C 0-3 -o ${RSF}/perf.data sleep 10 > /dev/null 2>&1
