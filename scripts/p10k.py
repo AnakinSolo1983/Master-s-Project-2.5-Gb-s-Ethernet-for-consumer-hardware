b8e8adc2ed5a36e167ae50cf8500ccd1813748ed
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 12:50:46 +0100

    Update p10k.py

--- scripts/p10k.py
+++ scripts/p10k.py
@@ -3,7 +3,11 @@
 # This is one of the Bash scripts used reads a configuration file containing network rules and generates corresponding iptables rules. The script processes each line of the file, extracts relevant information, and formats it into a command that can be executed in a Linux environment to manage network traffic. In this case, it accesses the 9785 rules from the fourth rule-set.
 
 #----------
+
+# Read the file with rules:
 with open('dts/dep/test-acl-input/acl1v4_10k_rule', 'r') as file:
+
+    # Extract the necessary fields of every rule.
     for line in file:
         parts = line.strip().split('\t')
         source = parts[0][1:]  # Remove the '@'
