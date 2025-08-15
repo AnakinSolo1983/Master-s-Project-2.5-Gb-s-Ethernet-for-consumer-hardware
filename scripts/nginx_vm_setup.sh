fcee8eede5aed62488aaeb9d4e2d3c07ede43ddd
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 12:45:17 +0100

    Update nginx_vm_setup.sh

--- scripts/nginx_vm_setup.sh
+++ scripts/nginx_vm_setup.sh
@@ -1,15 +1,11 @@
-#ip netns add ns1
+#----------
 
-#ip link set dev enp7s0 netns ns1
+# This Bash script is vital when testing for pcn-iptables. It configures the network interface for the server (VM), assigning an ip address to it.
+
+#----------
+
+# Enable the network interface enp7s0
 ip link set dev enp7s0 up
 
+# Assign the IP address 192.168.11.1 with a subnet mask of /24 to enp7s0:
 ip addr add dev enp7s0 192.168.11.1/24
-
-
-#ip netns add ns2
-
-#ip link set dev enp8s0 netns ns2
-
-#ip netns exec ns2 ip link set dev enp8s0 up
-
-#ip netns exec ns2 ip addr add dev enp8s0 192.168.11.10/24
