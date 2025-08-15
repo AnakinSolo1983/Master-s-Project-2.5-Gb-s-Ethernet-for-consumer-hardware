679ccae1bdbef7a17d488a2d43b3fdbe567ed151
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 13:06:53 +0100

    Update p_mod.py

--- scripts/p_mod.py
+++ scripts/p_mod.py
@@ -1,8 +1,19 @@
+#----------
+
+# This is a vital python script used reads a configuration file containing network rules and generates corresponding rules. The script processes each line of the file, extracts relevant information, and formats it into a command that can be executed in a Linux environment to manage network traffic. In this case, it accesses rules from the rule-set given by the parameter NUM. By analyzing a set of rules defined in a file, the script determines which rules should be applied and outputs them in a format suitable for use.
+
+#----------
+
 from ipaddress import IPv4Address, IPv4Network
 import sys
 
+# Creating a file NUM_mod_rule which will contain the chosen rules:
 rules = str(sys.argv[1]) + '_mod_rule'
+
+# Read the file with rules:
 with open(rules, 'r') as file:
+
+	# Extract the necessary fields of every rule:
 	for line in file:
         	parts = line.strip().split('\t')
         	source = parts[0][1:]  # Remove the '@'
@@ -13,12 +24,12 @@ with open(rules, 'r') as file:
         		continue
         
         	if source == '0.0.0.0/0' and source == dest:
-        		continue
+        		continue # Skipping rules where source IP address is 0.0.0.0/0 and is equal to destination IP address.
         
         	val1,val2 = dest_ports.split(':')
         	if int(val1) != int(val2):
                 	if int(val1) != 0 or int(val2) != 65535:
-                		continue
+                		continue # Skipping rules that do no meet this criteria.
         
         	rule = f"-A INPUT -s {source} -d {dest} -p tcp --sport {source_ports} --dport {dest_ports} -j DROP"
         	print(rule)
