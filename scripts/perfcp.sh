499b8668ff928d8c240373c8c9c1102f9b0b4e24
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 13:28:02 +0100

    Update perfcp.sh

--- scripts/perfcp.sh
+++ scripts/perfcp.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -x
 
-RSF=${1}
+#----------
 
+# This Bash script that handles file permissions for the perf data file stored in the VM, so that a copy of it can be transferred to the main machine, where the rest of the data for the test is stored.
+
+#----------
+
+RSF=${1} # Path to where data is stored.
+
+# List detailed information about the perf.data file before permission change:
 ls -l /home/olegvm/${RSF}/perf.data
+
+# Change the permissions of the perf.data file to read and write for the owner, and read for group and others:
 chmod 644 /home/olegvm/${RSF}/perf.data
+
+# List detailed information about the perf.data file after permission change:
 ls -l /home/olegvm/${RSF}/perf.data
