3381706a86644b3a87bed62c1f205d8e7fae8c7d
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 12:46:07 +0100

    Update nginx_vm_setup.sh

--- scripts/nginx_vm_setup.sh
+++ scripts/nginx_vm_setup.sh
@@ -4,7 +4,7 @@
 
 #----------
 
-# Enable the network interface enp7s0
+# Enable the network interface enp7s0:
 ip link set dev enp7s0 up
 
 # Assign the IP address 192.168.11.1 with a subnet mask of /24 to enp7s0:
