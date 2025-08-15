0900d7cf114354e7d1b1d6d9f390d01f37dfed0b
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 12:32:51 +0100

    Update nginx_kill.sh

--- scripts/nginx_kill.sh
+++ scripts/nginx_kill.sh
@@ -1,4 +1,10 @@
-#source nginxp.txt
-NGINXP=$(<nginxp.txt)
-kill ${NGINXP}
-ps -ef | grep nginx
+
+#----------
+
+# This Bash script only terminates the nginx process after the testing and verifies that it is done successfully.
+
+#----------
+
+NGINXP=$(<nginxp.txt) # Extract the process ID for nginx.
+kill ${NGINXP} # Terminate the process.
+ps -ef | grep nginx # Check that it is.
