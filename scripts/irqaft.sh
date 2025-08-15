df8b29d3d1320fa59b3499ab788ed2f44fa5b252
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 12:24:45 +0100

    Update irqaft.sh

--- scripts/irqaft.sh
+++ scripts/irqaft.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -x
 
-RSF=${1}
+#----------
+
+# This Bash script gathers information about the irqs for the Virtual Machine (VM) acting as server, after the testing.
+
+#----------
+
+RSF=${1} # Path to where the data will be stored.
 echo "ns1 After: " >> ${RSF}/sirqaft.out 2>&1
-cat /proc/interrupts | grep enp7s0-TxRx >> ${RSF}/sirqaft.out 2>&1
+cat /proc/interrupts | grep enp7s0-TxRx >> ${RSF}/sirqaft.out 2>&1 # Store it in a file sirqaft.out.
