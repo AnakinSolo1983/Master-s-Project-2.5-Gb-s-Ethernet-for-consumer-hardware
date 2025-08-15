4df5e33bda75de655fd56af1648268861da1e3e4
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 13:37:39 +0100

    Update xdp.sh

--- scripts/xdp.sh
+++ scripts/xdp.sh
@@ -1,9 +1,23 @@
+#----------
+
+# This Bash script is used to setup polycube, in order to write down rules into pcn-iptables within a VM, which acts as server. As a parameter, it takes the variable NUM, which is the number for one of the rule sets that it to be used in testing.
+
+#----------
+
 NUM=${1}
+
+# Go to polycube:
 cd polycube
+
+# Check that we are in polycube:
 pwd
 polycubed -d
 pwd
+
+Initiate pcn-iptables
 pcn-iptables-init-xdp
 pwd
+
+# Execute script that writes down the rules into pcn-iptables:
 /bin/bash -x testr${NUM}.sh
 
