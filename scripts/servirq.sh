a69fefee8c400f37e8e123530c5086e7a5f738a4
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 13:34:14 +0100

    Update servirq.sh

--- scripts/servirq.sh
+++ scripts/servirq.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -x
-RSF=${1}
+
+#----------
+
+# This is a vital Bash script which assigns irqs to the cores for server (VM). In addition it verifies that the assignment has been done. Also, it handles permissions to the data file so that it can be copied to the host machine where all data for the test will be stored.
+
+#----------
+
+RSF=${1} # Path to where the data is stored.
 
 SERVER=sudo lshw -c network -businfo
 
 rm -rf ${RSF}/*
 mkdir -p ${RSF}
+
 # See the information on interrupts assigned to server before:
 echo "ns1 Before: " >> ${RSF}/sirqstat.out 2>1
 cat /proc/interrupts | grep enp7s0-TxRx >> ${RSF}/sirqstat.out 2>1
@@ -13,13 +21,12 @@ cat /proc/interrupts | grep enp7s0-TxRx >> ${RSF}/sirqstat.out 2>1
 IRQS=($(cat /proc/interrupts | grep enp7s0-TxRx | awk '{print $1}' | tr -d :)); i=0; while [[ i -ne ${#IRQS[@]} ]]; 
 do 
 	echo "${i}: ${IRQS[${i}]}"; 
-	echo $i > /proc/irq/${IRQS[${i}]}/smp_affinity_list;  
-	#echo "0-3" > /proc/irq/${IRQS[${i}]}/smp_affinity_list;  
+	echo $i > /proc/irq/${IRQS[${i}]}/smp_affinity_list;    
 	cat /proc/irq/${IRQS[${i}]}/smp_affinity*; 
 	let i++;  
 done >> ${RSF}/sirqstat.out 2>1
 
-# Seeto which cores these interrupts are assigned:
+# See to which cores these interrupts are assigned:
 IRQS=($(cat /proc/interrupts | grep enp7s0-TxRx | awk '{print $1}' | tr -d :)); 
 i=0; 
 while [[ i -ne ${#IRQS[@]} ]]; 
