4d87a7a5110cb7bf4f899c8445664e8b9211d8cf
Oleg Ananiev <114877609+AnakinSolo1983@users.noreply.github.com>
2025-08-15 12:41:58 +0100

    Update nginx_start.sh

--- scripts/nginx_start.sh
+++ scripts/nginx_start.sh
@@ -1,26 +1,24 @@
-pwd
-#sudo -S /bin/bash
-#pwd
-#sudo -S ip netns exec ns1 nginx -c /home/olegvm/nginx.conf/nginx-cpus-cores-lp4.conf &
-#ip netns exec ns1 nginx -c /home/olegvm/nginx.conf/nginx-cpus-cores-lp4.conf
-#echo "Kamenka94" | sudo -S ip netns exec ns1 nginx -c /home/olegvm/nginx.conf/nginx-cpus-cores-lp4.con
+#----------
 
+# This Bash script is used to set up an nginx instance for Virtual Machine. It includes terminating all running instances of nginx before.
 
-# kill all running instances of nginx
+#----------
+
+pwd # Check our current location.
+
+# Kill all running instances of nginx:
 systemctl disable nginx
 systemctl stop nginx
-#systemctl status nginx
 
 kill $(pgrep nginx)
 
+# Verify that it worked:
 ps -ef | grep nginx
 
+# Start the server:
 nginx -c /home/olegvm/nginx.conf/nginx-cpus-cores-lp4.conf &
 NGINX_PID=$! # Get the PID.
 echo "Nginx PID: ${NGINX_PID}" 
-echo $NGINX_PID > nginxp.txt
+echo $NGINX_PID > nginxp.txt # Store the process ID into a file.
 sleep 5 # Give it time to setup.
-ps -ef | grep nginx
-
-#pwd
-
+ps -ef | grep nginx # Verify that it is running.
